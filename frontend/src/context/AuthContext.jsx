7a16efe9fec3c55928196b5ea2567b5df920facb
= <=>
2024-10-08 14:52:54 -0500

    Refactor AuthProvider component to handle user loading state and display loading message

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -6,26 +6,37 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loggedUser = authService.getLoggedUser();
     if (loggedUser) {
       setUser(loggedUser);
     }
+    setLoading(false);
   }, []);
 
   const login = async (credentials) => {
-    const loggedUser = await authService.login(credentials);
-    setUser(loggedUser);
+    try {
+      const loggedUser = await authService.login(credentials);
+      if (loggedUser.token) {
+        localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+        setUser(authService.getLoggedUser());
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+      throw error;
+    }
   };
 
   const logout = () => {
+    localStorage.removeItem('loggedUser');
     authService.logout();
     setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ user, setUser, login, logout }}>
+    <AuthContext.Provider value={{ user, setUser, login, logout, loading }}>
       {children}
     </AuthContext.Provider>
   );
