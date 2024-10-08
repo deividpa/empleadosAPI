e4a25a44355ade0632184adb38f08890bd28f57f
= <=>
2024-10-08 14:51:06 -0500

    Refactor EmpleadoList component to show alert for non-admin users

--- frontend/src/components/organisms/EmpleadoList.jsx
+++ frontend/src/components/organisms/EmpleadoList.jsx
@@ -3,7 +3,6 @@ import { EmpleadoContext } from '../../context/EmpleadoContext';
 import { AuthContext } from '../../context/AuthContext';
 import EmpleadoDetail from './EmpleadoDetail';
 import EmpleadoForm from '../molecules/EmpleadoForm';
-
 import Button from '../atoms/Button';
 
 const EmpleadoList = () => {
@@ -12,6 +11,7 @@ const EmpleadoList = () => {
   const [selectedEmpleado, setSelectedEmpleado] = useState(null);
   const [isDeleted, setIsDeleted] = useState(false);
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [showAlert, setShowAlert] = useState(user?.role !== 'admin');
 
   const handleSelectEmpleado = (empleado) => {
     setSelectedEmpleado(empleado);
@@ -20,6 +20,18 @@ const EmpleadoList = () => {
 
   return (
     <div className="container mx-auto p-4">
+      {showAlert && (
+        <div className="bg-orange-200 text-orange-800 p-4 rounded-lg mb-4 relative">
+          <span className="block">Solo los administradores pueden crear nuevos empleados.</span>
+          <button 
+            className="absolute top-0 right-0 mt-2 mr-4 font-bold text-orange-800" 
+            onClick={() => setShowAlert(false)}
+          >
+            x
+          </button>
+        </div>
+      )}
+      
       {user && user.role === 'admin' && (
         <div className="mt-4">
           <Button
@@ -31,19 +43,26 @@ const EmpleadoList = () => {
           {mostrarFormulario && <EmpleadoForm />}
         </div>
       )}
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-        {empleados.map((empleado) => (
+      {Array.isArray(empleados) && empleados.length > 0 ? (
+        empleados.map((empleado) => (
           <div
             key={empleado.id}
-            className="border p-4 rounded shadow cursor-pointer"
+            className="border p-4 rounded shadow cursor-pointer hover:bg-gray-100"
             onClick={() => handleSelectEmpleado(empleado)}
           >
+            <p className="text-lg text-blue-700"><strong>ID:</strong> {empleado.id}</p>
             <p><strong>Nombre:</strong> {empleado.nombre}</p>
             <p><strong>Fecha de Ingreso:</strong> {new Date(empleado.fechaIngreso).toLocaleDateString()}</p>
             <p><strong>Salario:</strong> {empleado.salario}</p>
           </div>
-        ))}
+        ))
+      ) : (
+        <p className="text-center text-gray-600">No hay empleados disponibles.</p>
+      )}
       </div>
+      
       {selectedEmpleado && (
         <EmpleadoDetail
           empleado={selectedEmpleado}
