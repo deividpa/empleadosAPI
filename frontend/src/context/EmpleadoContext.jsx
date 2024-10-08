cf2a1b8321b1864d61063639d405fcdeddcab27c
= <=>
2024-10-08 14:53:04 -0500

    Refactor EmpleadoContext to conditionally fetch and display employees based on user role

--- frontend/src/context/EmpleadoContext.jsx
+++ frontend/src/context/EmpleadoContext.jsx
@@ -1,19 +1,27 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import empleadoService from '../services/empleadoService';
+import { AuthContext } from './AuthContext';
 
 export const EmpleadoContext = createContext();
 
 export const EmpleadoProvider = ({ children }) => {
   const [empleados, setEmpleados] = useState([]);
+  const { user, loading } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchEmpleados = async () => {
-      const empleadosData = await empleadoService.getAllEmpleados();
-      setEmpleados(empleadosData);
+      if (!loading && user) {
+        try {
+          const empleadosData = await empleadoService.getAllEmpleados();
+          setEmpleados(empleadosData);
+        } catch (error) {
+          console.error('Error al obtener empleados:', error);
+        }
+      }
     };
     fetchEmpleados();
-  }, []);
+  }, [loading, user]);
 
   const addEmpleado = async (empleadoData) => {
     const newEmpleado = await empleadoService.createEmpleado(empleadoData);
