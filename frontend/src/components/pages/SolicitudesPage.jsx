84d9af91d5bbceb48e01e699feab0d166f5ee6cb
= <=>
2024-10-07 22:03:38 -0500

    Refactor SolicitudesPage component to remove unused code and simplify rendering

--- frontend/src/components/pages/SolicitudesPage.jsx
+++ frontend/src/components/pages/SolicitudesPage.jsx
@@ -1,23 +1,14 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { SolicitudContext } from '../../context/SolicitudContext';
 import SolicitudList from '../organisms/SolicitudList';
-import SolicitudDetail from '../organisms/SolicitudDetail';
 
 const SolicitudesPage = () => {
   const { solicitudes } = useContext(SolicitudContext);
-  const [selectedSolicitud, setSelectedSolicitud] = useState(null);
-
-
-  const handleSelectSolicitud = (solicitudId) => {
-    const solicitud = solicitudes.find((s) => s.id === solicitudId);
-    setSelectedSolicitud(solicitud);
-  };
 
   return (
     <div className="mx-5">
       <h1 className="w-full p-5 bg-slate-200">Solicitudes</h1>
-      <SolicitudList solicitudes={solicitudes} onSelect={handleSelectSolicitud} />
-      {selectedSolicitud && <SolicitudDetail solicitud={selectedSolicitud} /> }
+      <SolicitudList solicitudes={solicitudes} />
     </div>
   );
 };
