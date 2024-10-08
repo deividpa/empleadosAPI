deb9d7456dfc1353be8c1e74aa008fd3be65b141
= <=>
2024-10-07 23:54:04 -0500

    Refactor Dockerfile to execute Prisma migrations before starting the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 
 // Rutas
 app.use('/api/empleados', empleadoRoutes);
